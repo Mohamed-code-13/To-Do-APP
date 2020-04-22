a71e777510bb6eb8fb63dfe9a6672d7168c1d469
Mohamed-code-13 <mody_nnn25@yahoo.com>
2020-04-22 14:38:44 +0200

    Adding a style file. Changing The Style.

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Todo(models.Model):
+    objects = None
     text = models.CharField(max_length=200)
-    time = models.DateTimeField(default=timezone.now())
+    time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.text
--- ToDoList/views.py
+++ ToDoList/views.py
@@ -3,22 +3,24 @@ from .models import Todo
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+
 # Create your views here.
 def home(request):
     todo_items = Todo.objects.all().order_by('-time')
-
 
     return render(request, 'todoapp/index.html', {
         'todo_items': todo_items,
     })
 
+
 def add_todo(request):
     current_date = timezone.now()
     content = request.POST['content']
 
-    create_obj = Todo.objects.create(text=content, time=current_date)
+    Todo.objects.create(text=content, time=current_date)
 
     return HttpResponseRedirect("/")
+
 
 def delete_todo(request, id_todo):
     Todo.objects.get(id=id_todo).delete()
